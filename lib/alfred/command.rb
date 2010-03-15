706fcc403e3da56e14effbdd43e516bb3e99fd1a
Dale Campbell <oshuma@gmail.com>
2010-03-14 20:43:20 -0500

    Alfred::CommandError

--- lib/alfred/command.rb
+++ lib/alfred/command.rb
@@ -8,6 +8,8 @@ if false
 end
 
 module Alfred
+  class CommandError < StandardError; end
+
   class Command
 
     class << self
@@ -15,7 +17,27 @@ module Alfred
       def exec(raw_string)
         %x[#{raw_string}].chomp
       end
+    end # self
+
+    attr_accessor :name
+    attr_reader :raw
+
+    # Use as such:
+    #
+    #   Command.new('Host', 'hostname')
+    #   # ..or..
+    #   Command.new({ :name => 'Host', :exec => 'hostname' })
+    def initialize(*args)
+      if args.first.is_a?(Hash)
+        options = args.first
+        @name = options[:name]
+        @raw  = options[:exec]
+      else
+        @name = args[0]
+        @raw  = args[1]
+      end
+      raise Alfred::CommandError unless @raw
     end
 
-  end
-end
+  end # Command
+end # Alfred
--- spec/alfred/command_spec.rb
+++ spec/alfred/command_spec.rb
@@ -3,7 +3,34 @@ require 'spec_helper'
 describe Alfred::Command do
 
   it 'can execute arbitrary shell commands' do
-    Command.exec('hostname').should_not be_nil
+    Command.exec('hostname').should == %x[hostname].chomp
   end
+
+  it '#initialize accepts an array' do
+    command = Command.new('Host', 'hostname')
+    command.name.should == 'Host'
+  end
+
+  it '#initialize accepts a hash' do
+    command = Command.new({:name => 'Host', :exec => 'hostname'})
+    command.name.should == 'Host'
+  end
+
+  it 'complains if missing raw command input' do
+    lambda do
+      Command.new('no command')
+    end.should raise_error(Alfred::CommandError)
+  end
+
+  describe 'instance' do
+    before(:each) do
+      @options = {:name => 'Host', :exec => 'hostname'}
+      @command = Command.new(@options)
+    end
+
+    it 'stores the raw command string' do
+      @command.raw.should == @options[:exec]
+    end
+  end # instance
 
 end
