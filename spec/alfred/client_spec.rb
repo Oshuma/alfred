ca22d51b6ec08f0a128a364bfe1bc53d834ae954
Dale Campbell <oshuma@gmail.com>
2010-03-16 17:17:02 -0500

    client spec

--- spec/alfred/client_spec.rb
+++ spec/alfred/client_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Alfred::Client do
+  it 'should remove the command from the processed args' do
+    args   = ['report', '-h', '80']
+    client = Client.new(args)
+    client_args = client.instance_variable_get(:@args)
+    client_args.should_not include('report')
+  end
 end
