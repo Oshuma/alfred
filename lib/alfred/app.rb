9f6e772aecae73b8d60a3e10c711870432c67aa6
Dale Campbell <oshuma@gmail.com>
2010-03-16 15:57:41 -0500

    command stuff

--- lib/alfred/app.rb
+++ lib/alfred/app.rb
@@ -2,12 +2,17 @@ require 'sinatra/base'
 require 'erb'
 
 module Alfred
+  # TODO: Split the views into subdirs.
   class App < Sinatra::Base
 
     set :root, "#{File.dirname(__FILE__)}/../../app"
     enable :static
 
     before do
+      if request.env['HTTP_USER_AGENT'] =~ /^(curl\/[\d\.]+)/
+        headers['Content-Type'] = 'text/plain'
+        @text_request = true
+      end
       @hostname = Alfred::Command.exec('hostname')
       @command_file = File.dirname(__FILE__) + '/../../config/commands.yml'
       @commands = Alfred::Command.from_yaml(@command_file)
@@ -16,21 +21,25 @@ module Alfred
     helpers do
       include Rack::Utils
       alias_method :h, :escape_html
-
-      def anchor_name(string)
-        link = string.gsub(/\s/, '_').downcase
-        link
-      end
-    end
-
-    # Text representation (for curling).
-    get '/', :agent => /^(curl\/[\d\.]+)/ do
-      headers['Content-Type'] = 'text/plain'
-      erb :index_text, :layout => false
     end
 
     get '/' do
-      erb :index
+      @text_request ? erb(:index_text, :layout => false) : erb(:index)
+    end
+
+    get '/commands' do
+      if @text_request
+        @commands.map { |c| "#{c.name} (#{c.id})" }.join("\n")
+      else
+        erb :commands
+      end
+    end
+
+    [ '/command/:id', '/c/:id' ].each do |command_path|
+      get command_path do
+        @command = @commands.select { |c| params[:id] == c.id }.first
+        @text_request ? @command.output : erb(:command)
+      end
     end
 
   end
--- spec/alfred/app_spec.rb
+++ spec/alfred/app_spec.rb
@@ -11,6 +11,28 @@ describe Alfred::App do
   it 'responds with text/plain if curling' do
     header 'User-Agent', 'curl/7.16.3'
     get '/'
+    last_response.should be_ok
     last_response.headers['Content-Type'].should == 'text/plain'
   end
+
+  describe '/command' do
+    before(:each) do
+      @command_file = File.dirname(__FILE__) + '/../../config/commands.spec.yml'
+      @commands = Command.from_yaml(@command_file)
+      @command = @commands.first
+    end
+
+    it 'shows the command output' do
+      get "/command/#{@command.id}"
+      last_response.should be_ok
+    end
+
+    it 'responds with text/plain if curling' do
+      header 'User-Agent', 'curl/7.16.3'
+      get "/command/#{@command.id}"
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'text/plain'
+    end
+  end # /command
+
 end
