5015c97bc222ddcdc8fa046823c8ed9767cebbde
Dale Campbell <oshuma@gmail.com>
2013-07-09 20:54:30 -0400

    refactoring the auth shit

--- lib/alfred/app.rb
+++ lib/alfred/app.rb
@@ -20,46 +20,61 @@ module Alfred
     helpers do
       include Rack::Utils
       alias_method :h, :escape_html
-    end
 
-    before do
-      if Alfred.config.auth_token
+      def check_auth_token!
+        return unless Alfred.config.auth_token
+
         auth_token = request.env['HTTP_X_AUTH_TOKEN']
 
-        halt 401 if auth_token.nil?
-        halt 401 unless auth_token == Alfred.config.auth_token
-      elsif Alfred.config.username
+        error 401 if auth_token.nil?
+        error 401 unless auth_token == Alfred.config.auth_token
+      end
+
+      def check_basic_auth!
+        return unless Alfred.config.username
+
         self.class.use Rack::Auth::Basic, "Restricted Area" do |username, password|
           (username == Alfred.config.username) && (password == Alfred.config.password)
         end
       end
+    end
 
+    before do
       @hostname = %x[hostname]
       @commands = Alfred::Command.all
     end
 
+    # GET /
+    # GET /commands
     [ '/', '/commands' ].each do |root_path|
       get root_path do
+        check_basic_auth!
         erb(:index)
       end
     end
 
+    # GET /c/:id
+    # GET /command/:id
     [ '/command/:id', '/c/:id' ].each do |command_path|
       get command_path do
+        check_basic_auth!
         @command = Alfred::Command.find(params[:id])
         erb(:command)
       end
     end
 
     # API: Authenticate
-    # This exists to give the client a positive result, since Unauthorized
-    # will be throw in the `before` block if authentication fails.
+    # GET: /api/authenticate.json
     get '/api/authenticate.json' do
+      check_auth_token!
+      headers 'Content-Type' => 'application/json'
       status 202 # Accepted
     end
 
     # API: Commands
+    # GET: /api/commands.json
     get '/api/commands.json' do
+      check_auth_token!
       headers 'Content-Type' => 'application/json'
       @commands.to_json
     end
